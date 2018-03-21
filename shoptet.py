1dcf12cc6c1525b4e316d0beda5e19136aaba76b
mimmon <juraj.bezrucka@gmail.com>
2018-03-21 23:49:37 +0100

    added methods for browsing orders

--- shoptet.py
+++ shoptet.py
@@ -1,36 +1,83 @@
 #!venv/bin/python
 # -*- coding: utf-8 -*-
+import logging
+from lxml import etree, html
+import os
 import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-BASE_URL = 'YOUR-URL'
+_logger = logging.getLogger(__name__)
+
+CONFIG_FILE = '/home/mimmon/.shoptet'
+
+def parse_config(filename):
+    # KEY=VALUE
+    # left side of the first = is key, the rest is value, may contain =s
+    result = {}
+    with open(filename, 'r') as f:
+        for line in filter(lambda x: '=' in x, f.readlines()):
+            try:
+                key, value = line.split('=', 1)
+                result[key.strip()] = value.strip()
+            except ValueError:
+                # skip line
+                _logger.info('skipping invalid line: %s', line)
+    return result
+
+
+config = parse_config(CONFIG_FILE)
+
+BASE_URL = config.get('BASE_URL', 'YOUR-URL')
+BASE_URL = BASE_URL + os.sep if not BASE_URL.endswith(os.sep) else ''
 ADMIN_URL = BASE_URL + 'admin/'
 LOGIN_URL = ADMIN_URL + 'login/'
 
-# CREDENTIALS: TODO READ FROM SYMLINK
-EMAIL = 'EMAIL'
-PASSWORD = 'PASSWORD'
+EMAIL = config.get('EMAIL', 'EMAIL')
+PASSWORD = config.get('PASSWORD', 'PASSWORD')
+
+ORDER_LINK = ADMIN_URL + 'objednavky-detail/?id={order_id}'
+
 
 def get_credentials():
-    return {'email': EMAIL, 'password': PASSWORD}
+    conf = parse_config('~/.shoptet')
+    return {k: v for k, v in conf.items() if k.lower() in ['email', 'password']}
 
 
-class ShoptetBrowsing:
+def login_required(func):
+    """
+    Checks if user is logged in.
+    If not, login will precede the method execution.
+    """
+    def wrapper(*args, **kwargs):
+        self = args[0]
+        if not self.logged_in:
+            self.login()
+        return func(*args, **kwargs)
+    return wrapper
+
+
+class Shoptet:
     browser = None
+    logged_in = None
 
     def __init__(self):
         self.browser = webdriver.Firefox()
         # browser = webdriver.PhantomJS()
 
-    def find_pattern(self, pattern, get_one=False):
+    def find_pattern(self, pattern, get_first=False):
         found = re.findall(pattern, self.browser.page_source)
-        if get_one and found:
+        if get_first and found:
             return found[0]
         return found
 
-    def login(self):
+    def find_by_xpath(self, xpath):
+        return self.browser.find_element(By.XPATH, xpath)
 
+    def find_by_css(self, selector):
+        return self.browser.find_element(By.CSS_SELECTOR, selector)
+
+    def login(self):
         self.browser.get(ADMIN_URL)
         # tokens = re.findall(r'shoptet.csrfToken = "(?P<token>.+)";', browser.page_source)
         # csrf_token = tokens and tokens[0] or None
@@ -43,6 +90,8 @@ class ShoptetBrowsing:
 
         el = self.browser.find_element(By.XPATH, '//input[@class="fake-submit"]')
         el.click()
+
+        self.logged_in = True
 
     def get_orders(self):
 
@@ -64,34 +113,71 @@ class ShoptetBrowsing:
     def get_max_page(self):
         self.get_order_page()
 
-        max_page = self.find_pattern(
-            r'<a href=.+title="Posledná stránka".+>(?P<max_page>\d)</a>',
-            get_one=True)
+        max_page = 1
+        last_link = self.find_by_xpath("//a[@title='Posledná stránka']")
+        if last_link:
+            max_page = int(getattr(last_link, 'text', 0))
         return max_page
 
-    def get_order_link(self, order_num):
-        pass
-
-    def get_page_extreme_orders(self, page_num=None, reload=True):
-        if reload:
+    def browse_orders(self, page_num=None):
+        if page_num:
             self.get_order_page(page_num)
-        tbody = self.browser.find_element(
-            By.XPATH, "//div[@class='table-holder']/table/tbody")
+        # browse orders on page
+        # wip - this only get the first one
+        # self.browser.find_element(By.CSS_SELECTOR, "tr[id*='rowId-']")
+        # html = etree.parse(self.browser.page_source, parser=etree.HTMLParser(remove_comments=True))
+        doc = html.document_fromstring(self.browser.page_source)
+        order_ids = map(int, self.find_pattern('<tr id="rowId-(?P<order_id>\d+)'))
+        sorted_list = sorted(order_ids)
+
+        for order_id in sorted_list:
+            row = doc.get_element_by_id('rowId-{}'.format(order_id))
+            a = row.find('td/a[@title="Detail objednávky"]')
+            url = a.attrib.get('href')
+            order_num = a.text
+            customer_name = row.find('td/div/div[@class="order-customer"]').text.strip()
+
+            print('ORDER {}: {}'.format(order_num, customer_name))
 
 
     def get_order(self, order_number):
         pass
-        # el = self.browser.find_element(By.XPATH, "//a[@href='/admin/objednavky/']")
-        # el.click()
-        #
-        # el = self.browser.find_element(By.XPATH, "//a[@href='/admin/prehlad-objednavok/']")
-        # el.click()
 
-        # browser.get('https://www.bio-market.sk/admin/')
+
+    def get_order_from_link(self, order_id):
+        if not self.logged_in:
+            self.login()
+
+    @login_required
+    def get_last_order_id(self):
+        # if not self.logged_in:
+        #     self.login()
+        self.get_order_page()
+        last_order_id = sh.find_pattern('<tr id="rowId-(?P<order_id>\d+)', get_first=True)
+        print('last_order %s' % last_order_id)
+        return int(last_order_id)
+
 
 if __name__ == '__main__':
-    sh = ShoptetBrowsing()
+    sh = Shoptet()
     sh.login()
-    sh.get_order_page(3)
-    sh.get_max_page()
+    max_page = sh.get_max_page()
+    sh.browse_orders(max_page)
+
+    # TO BE UNCOMMENTED WHEN DB IS FINISHED
+    # for page in range(max_page, 0):
+    #     sh.browse_orders(page)
+    sh.get_last_order_id()
+
     pass
+
+
+## ADD ARGUMENT PARSER
+
+## SPLIT TO VARIOUS FUNCTIONS
+
+#### parse orders + write to db
+#### get a single order detail + make it parse through all of them
+#### parse vouchers
+#### check voucher
+
