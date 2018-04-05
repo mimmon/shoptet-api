7bc6fb7f8586a44e0b4aeb19d0be4e3263b4bfbe
Juraj M. Bezrucka <mimmon@gmail.com>
2018-04-05 22:19:14 +0200

    get order details from link, moved multiple utils to separate file

--- shoptet.py
+++ shoptet.py
@@ -1,5 +1,7 @@
 #!venv/bin/python
 # -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+from decimal import Decimal
 import logging
 from lxml import etree, html
 import os
@@ -7,54 +9,22 @@ import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from .utils import *
+
 _logger = logging.getLogger(__name__)
 
 CONFIG_FILE = '/home/mimmon/.shoptet'
-
-def parse_config(filename):
-    # KEY=VALUE
-    # left side of the first = is key, the rest is value, may contain =s
-    result = {}
-    with open(filename, 'r') as f:
-        for line in filter(lambda x: '=' in x, f.readlines()):
-            try:
-                key, value = line.split('=', 1)
-                result[key.strip()] = value.strip()
-            except ValueError:
-                # skip line
-                _logger.info('skipping invalid line: %s', line)
-    return result
-
-
 config = parse_config(CONFIG_FILE)
 
 BASE_URL = config.get('BASE_URL', 'YOUR-URL')
 BASE_URL = BASE_URL + (os.sep if not BASE_URL.endswith(os.sep) else '')
-ADMIN_URL = BASE_URL + 'admin/'
-LOGIN_URL = ADMIN_URL + 'login/'
+ADMIN_URL = BASE_URL + '/admin'
+LOGIN_URL = ADMIN_URL + '/login'
 
 EMAIL = config.get('EMAIL', 'EMAIL')
 PASSWORD = config.get('PASSWORD', 'PASSWORD')
 
 ORDER_LINK = ADMIN_URL + 'objednavky-detail/?id={order_id}'
-
-
-def get_credentials():
-    conf = parse_config('~/.shoptet')
-    return {k: v for k, v in conf.items() if k.lower() in ['email', 'password']}
-
-
-def login_required(func):
-    """
-    Checks if user is logged in.
-    If not, login will precede the method execution.
-    """
-    def wrapper(*args, **kwargs):
-        self = args[0]
-        if not self.logged_in:
-            self.login()
-        return func(*args, **kwargs)
-    return wrapper
 
 
 class Shoptet:
@@ -101,13 +71,11 @@ class Shoptet:
         el = self.browser.find_element(By.XPATH, "//a[@href='/admin/prehlad-objednavok/']")
         el.click()
 
-        # browser.get('https://www.bio-market.sk/admin/')
-
     def get_order_page(self, page_num=None):
         if page_num is None:
             page_num = 1
         appendix = ('?from=%d' % page_num) if page_num > 1 else ''
-        url = ADMIN_URL + 'prehlad-objednavok/%s' % appendix
+        url = ADMIN_URL + '/prehlad-objednavok/%s' % appendix
         self.browser.get(url)
 
     def get_max_page(self):
@@ -119,6 +87,7 @@ class Shoptet:
             max_page = int(getattr(last_link, 'text', 0))
         return max_page
 
+    @login_required
     def browse_orders(self, page_num=None):
         if page_num:
             self.get_order_page(page_num)
@@ -127,45 +96,180 @@ class Shoptet:
         # self.browser.find_element(By.CSS_SELECTOR, "tr[id*='rowId-']")
         # html = etree.parse(self.browser.page_source, parser=etree.HTMLParser(remove_comments=True))
         doc = html.document_fromstring(self.browser.page_source)
-        order_ids = map(int, self.find_pattern('<tr id="rowId-(?P<order_id>\d+)'))
+        order_ids = map(int,
+                        self.find_pattern('<tr id="rowId-(?P<order_id>\d+)'))
         sorted_list = sorted(order_ids)
+
+        orders = []
 
         for order_id in sorted_list:
             row = doc.get_element_by_id('rowId-{}'.format(order_id))
             a = row.find('td/a[@title="Detail objednávky"]')
             url = a.attrib.get('href')
             order_num = a.text
-            customer_name = row.find('td/div/div[@class="order-customer"]').text.strip()
+            customer_name = row.find(
+                'td/div/div[@class="order-customer"]').text.strip()
 
             print('ORDER {}: {}'.format(order_num, customer_name))
 
+            orders.append({'order_number': order_num, 'order_id': order_id, 'order_url': url})
 
-    def get_order(self, order_number):
+        return orders
+
+    @login_required
+    def get_order_from_link(self, url, save=False):
+        """Open url and scrape the order, optionally store to database"""
+        if not url:
+            _logger.warning('No url for order %s', order)
+            return None
+
+        order_details = {}
+        shop.browser.get(BASE_URL + url)
+        soup = BeautifulSoup(self.browser.page_source, 'html5lib')
+
+        # CONTACT
+        uinfo = order_details.setdefault('user_info', {})
+        email = None
+        contact = soup.find('td', {'id': 'customer-contact'})
+        try:
+            email = contact.p.a['href'].split(':', 1)[1]
+        except:
+            _logger.info('Email cannot be found')
+        uinfo.update({'email': email})
+
+        phone = None
+        for line in filter(lambda x: 'Telefón' in x, contact.p.contents):
+            phones = re.findall('Telefón: *(\d+)?', line)
+            if phones:
+                phone = phones[0]
+                break
+        uinfo.update({'phone': phone})
+
+        # BILLING
+        billing = soup.find('td', {'id': 'billing-address'})
+        name_element = billing.find('a', {'title': 'Detail zákazníka'})
+        name = name_element.text if name_element else None
+        uinfo.update({'name': name})
+
+        # as the address in order details is stored as (more or less) plain text
+        # in this version of program it will be ignored
+        # in the future there can be a  individual scraper for users only
+        # users may be identified by email
+        # if save:
+        #     User.get()
+        #       if not user:
+        #             create_user()
+        #       Order.get()
+        #         if not order:
+        #           create_order()
+        #         else:
+        #            update_order()
+
+        ##############################################################
+        # MISSING PARAMS
+        # street = CharField(null=True)
+        # zip = CharField(null=True)
+        # city = CharField(null=True)
+        # country = CharField(null=True)
+        # phone = CharField(null=True)
+
+        order_details.setdefault('order_lines', [])
+        # we need to save total price, transport price, plain price, discount applied
+        # all of them including and excluding vat
+
+        # postage/transport prices have no status and no code
+        # discount
+        # total price with and without vat can be taken from summary below order
+        div_total_price = soup.find('div', {'class': 'total-price'})
+        for line in filter_contents(div_total_price):
+            for keyword, content, contra in [
+                    ('no_vat_price', 'Cena bez DPH:', None),
+                    ('vat', 'DPH:', 'Cena bez DPH:'),
+                    ('total_price', 'Čiastka k úhrade:', None)]:
+                # total price is not navigable string, but Tag (<big>)
+                if not isinstance(line, str):
+                    line = line.contents[0]
+                if content in line and (contra is None or contra not in line):
+                    order_details[keyword] = strip_price(line, content)
+                    break
+
+        table = soup.find('table', {'class': 'std-table-listing'})
+        # todo add mapping from thead
+        for tr in table.tbody.find_all('tr'):
+            # for now we ignore individual items
+            # this iteration is to determine postage and transport prices
+            tds = tr.find_all('td')
+            p_code = handle_element(tds[0], 'a', lambda x: x.text.strip())
+            p_img = handle_element(tds[1], 'img', lambda x: x['src'])
+            p_description = handle_element(tds[2], 'span', lambda x: x.text)
+            p_status = handle_contents(tds[3])
+            # todo add units to amount
+            p_amount = handle_contents(tds[4], lambda x: len(x.split()) > 1 and x.split()[0] else x)
+            p_unit_price = handle_content_price(tds[5])
+            p_discount_percent = handle_contents(
+                tds[6], lambda x: x.span.text.strip().rstrip(' %'), Decimal)
+            p_vat_percent = handle_contents(
+                tds[7], lambda x: x.strip().rstrip('%'), Decimal)
+            p_total_vat_including = handle_content_price(tds[8])
+            p_del = handle_contents(tds[9])
+
+            # todo store product lines and purchases for further analysis
+            # first step requires distinguishing between products and services
+            # (postage, pay-on-delivery)
+            # services are "products" with no code, image and status
+            # services should be negative - that is reserved for total_discount
+            order_details['order_lines'].append({
+                'code': p_code,
+                'img': p_img,
+                'description': p_description,
+                'status': p_status,
+                'amount': p_amount,
+                'unit_price': p_unit_price,
+                'discount_percent': p_discount_percent,
+                'vat_percent': p_vat_percent,
+                'total_vat_including': p_total_vat_including,
+            })
+
+        # TODO create function in utils using peewee ORM
+        # if save:
+        #     save_order(order_details)
+
+        return order_details
+
+    def get_order(self, order_number, save=False):
+        """Get link from DB and call get_order_from_link, optionally store to database"""
         pass
-
-
-    def get_order_from_link(self, order_id):
-        if not self.logged_in:
-            self.login()
 
     @login_required
     def get_last_order_id(self):
         self.get_order_page()
-        last_order_id = sh.find_pattern('<tr id="rowId-(?P<order_id>\d+)', get_first=True)
+        last_order_id = self.find_pattern('<tr id="rowId-(?P<order_id>\d+)', get_first=True)
         print('last_order %s' % last_order_id)
         return int(last_order_id)
 
+    @login_required
+    def get_user_by_id(self):
+        pass
+
+    def get_user_by_email(self):
+        # get from db
+        pass
+
 
 if __name__ == '__main__':
-    sh = Shoptet()
-    sh.login()
-    max_page = sh.get_max_page()
-    sh.browse_orders(max_page)
+    shop = Shoptet()
+    shop.login()
+    max_page = shop.get_max_page()
+    orders = shop.browse_orders(max_page)
 
+    for order in orders[:5]:
+        url = order.get('order_url')
+        print(url)
+        shop.get_order_from_link(url)
     # TO BE UNCOMMENTED WHEN DB IS FINISHED
     # for page in range(max_page, 0):
     #     sh.browse_orders(page)
-    sh.get_last_order_id()
+    shop.get_last_order_id()
 
     pass
 
---
+++ utils.py
@@ -0,0 +1,144 @@
+from decimal import Decimal
+import logging
+
+from .models import Customer
+
+_logger = logging.getLogger(__name__)
+
+
+def parse_config(filename):
+    # KEY=VALUE
+    # left side of the first = is key, the rest is value, may contain =s
+    result = {}
+    with open(filename, 'r') as f:
+        for line in filter(lambda x: '=' in x, f.readlines()):
+            try:
+                key, value = line.split('=', 1)
+                result[key.strip()] = value.strip()
+            except ValueError:
+                # skip line
+                _logger.info('skipping invalid line: %s', line)
+    return result
+
+
+def get_credentials():
+    conf = parse_config('~/.shoptet')
+    return {k: v for k, v in conf.items() if k.lower() in ['email', 'password']}
+
+
+def login_required(func):
+    """
+    Checks if user is logged in.
+    If not, login will precede the method execution.
+    """
+    def wrapper(*args, **kwargs):
+        self = args[0]
+        if not self.logged_in:
+            self.login()
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def handle_element(elem, args, func=None, func2=None):
+    """
+    A helper function to handle elements from BeautifulSoup element.find()
+    :param elem: BeautifulSoup Tag (element)
+    :param args: args tuple for find method. String can be used for a single
+            arg, otherwise tuple
+    :param func: ad hoc function used to manipulate found element
+    :param func2: outer function, may be used to convert the result found in
+            the process
+    :return: content of found element processed by func or None if not found
+    """
+    if elem is None:
+        return elem
+    if isinstance(args, str):
+        found = elem.find(args)
+    elif isinstance(args, (tuple, list)):
+        found = elem.find(*args)
+    else:
+        found = elem
+    if not found:
+        return None
+    if not func or not callable(func):
+        if not callable(func):
+            _logger.warning('%s not callable', func)
+        return found
+    _found = func(found)
+    if not func2 or not callable(func2):
+        return _found
+    try:
+        return func2(_found)
+    except ValueError:
+        return _found
+
+
+def handle_contents(elem, func=None, func2=None):
+    """
+    Helper function to extract data from a single line / unstructure entry
+    :param elem: BeautifulSoup Tag (element)
+    :param func: ad hoc function used to manipulate content
+    :param func2: outer function, may be used to convert the result
+    :return: content of content processed by func or None if not found
+    """
+    return handle_element(elem.contents[0].strip(), None, func, func2)
+
+
+def handle_content_price(elem, clean=None):
+    if clean is not None and callable(clean):
+        elem = clean(elem)
+    return handle_contents(
+        elem, lambda x: x.lstrip('€').replace(',', '.'), Decimal)
+
+
+def handle_price(line, clean=None):
+    if clean is not None and callable(clean):
+        line = clean(line)
+    price = line.lstrip('€').replace(',', '.')
+    try:
+        return Decimal(price)
+    except ValueError:
+        return None
+
+
+def strip_price(line, strip_string=None):
+    line = line.strip()
+    if strip_string:
+        line = line.strip(strip_string).strip()
+    price = line.lstrip('€').replace(',', '.')
+    try:
+        return Decimal(price)
+    except ValueError:
+        return None
+
+
+def filter_contents(elem):
+    f = lambda x: isinstance(x, str) or getattr(x, 'name', None) != 'br'
+    for i in filter(f, elem.contents):
+        yield i
+
+
+# DATABASE HANDLING
+def save_order(order_details):
+    uinfo = order_details.get('user_info')
+    customer = Customer.get_or_create(**uinfo)
+
+    order = Order()
+    for order_line in order_details.get('order_lines', []):
+        order_id = order.id
+        OrderLine()
+
+
+    if uinfo:
+        # find if user exists in database
+        if 'email' in uinfo:
+            Customer.get(email=uinfo['email'])
+        elif 'code' in uinfo:
+            Customer.get(code=uinfo['code'])
+
+        for key in ['email', 'code']:
+            if key in uinfo:
+                Customer.get(**{key: uinfo[key]})
+                break
+        else:
+            raise MissingKeyError
